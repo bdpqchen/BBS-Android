50f2276781f369d7cff0115bffa5a0924c73956c
bdpqchen <bdpqchen@gmail.com>
2017-04-17 18:46:26 +0800

    twtBBS-Android端仓库初始化

---
+++ BBS/app/src/main/java/com/twtstudio/bbs/bdpqchen/bbs/App.java
@@ -0,0 +1,16 @@
+package com.twtstudio.bbs.bdpqchen.bbs;
+
+import android.app.Application;
+
+/**
+ * Created by bdpqchen on 17-4-17.
+ */
+
+public class App extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+
+    }
+}
