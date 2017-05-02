d31f6c5097fc05231f43d9d8157e973cbe5873cd
bdpqchen <bdpqchen@gmail.com>
2017-05-02 21:10:03 +0800

    添加启动页面

--- BBS/app/src/main/java/com/twtstudio/bbs/bdpqchen/bbs/commons/utils/PrefUtil.java
+++ BBS/app/src/main/java/com/twtstudio/bbs/bdpqchen/bbs/commons/utils/PrefUtil.java
@@ -8,10 +8,13 @@ import com.orhanobut.hawk.Hawk;
 
 public final class PrefUtil {
 
+    //
     private static final String IS_JUST_INSTALLED = "is_just_installed";
     private static final String IS_NIGHT_MODE = "is_night_mode";
     private static final String IS_SLIDE_BACK_MODE = "is_slide_back_mode";
     private static final String IS_AUTO_NIGHT_MODE = "is_auto_night_mode";
+    private static final String HAD_LOGIN = "had_login";
+    private static final String IS_NO_ACCOUNT_USER = "is_no_account_user";
 
     public static boolean isJustInstalled(){
         return Hawk.get(IS_JUST_INSTALLED, false);
@@ -40,5 +43,22 @@ public final class PrefUtil {
     public static void setIsAutoNightMode(boolean b){
         Hawk.put(IS_AUTO_NIGHT_MODE, b);
     }
+
+    public static boolean hadLogin() {
+        return Hawk.get(HAD_LOGIN, false);
+    }
+    public static void setHadLogin(boolean b){
+        Hawk.put(HAD_LOGIN, b);
+    }
+
+    public static boolean isNoAccountUser() {
+        return Hawk.get(IS_NO_ACCOUNT_USER, false);
+    }
+
+    public static void setIsNoAccountUser(boolean b){
+        Hawk.put(IS_NO_ACCOUNT_USER, b);
+    }
+
+
 
 }
