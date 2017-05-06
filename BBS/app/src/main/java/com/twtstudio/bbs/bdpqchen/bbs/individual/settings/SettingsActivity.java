7785f785f44442fb02b5163d383e599a084c9301
bdpqchen <bdpqchen@gmail.com>
2017-05-06 11:34:53 +0800

    解决夜间模式切换问题，但是所在Activity不能支持滑动返回

--- BBS/app/src/main/java/com/twtstudio/bbs/bdpqchen/bbs/individual/settings/SettingsActivity.java
+++ BBS/app/src/main/java/com/twtstudio/bbs/bdpqchen/bbs/individual/settings/SettingsActivity.java
@@ -6,14 +6,18 @@ import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
+import android.support.v7.widget.SwitchCompat;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CompoundButton;
 import android.widget.FrameLayout;
 import android.widget.TextView;
 
 import com.twtstudio.bbs.bdpqchen.bbs.R;
 import com.twtstudio.bbs.bdpqchen.bbs.commons.base.BaseActivity;
+import com.twtstudio.bbs.bdpqchen.bbs.commons.manager.ActivityManager;
+import com.twtstudio.bbs.bdpqchen.bbs.commons.utils.HandlerUtil;
 import com.twtstudio.bbs.bdpqchen.bbs.commons.utils.LogUtil;
 import com.twtstudio.bbs.bdpqchen.bbs.commons.utils.PrefUtil;
 import com.twtstudio.bbs.bdpqchen.bbs.test.SecondActivity;
@@ -27,6 +31,7 @@ import butterknife.OnClick;
 
 public class SettingsActivity extends BaseActivity {
 
+    private static final String IS_SWITCH_NIGHT_MODE = "isSwitchNightMode";
     @BindView(R.id.toolbar)
     Toolbar mToolbar;
     @BindView(R.id.fl_settings_container_1)
@@ -35,6 +40,8 @@ public class SettingsActivity extends BaseActivity {
     TextView mTvLogout;
     @BindView(R.id.btn_start_second)
     Button mBtnStartSecond;
+    @BindView(R.id.switch_night_mode)
+    SwitchCompat mSwitchNightMode;
 
     private Activity mActivity;
 
@@ -72,6 +79,10 @@ public class SettingsActivity extends BaseActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+
+        //本Activity不支持滑动返回，当前使用的滑动返回库不太友好
+        mSlideBackLayout.lock(getIntent().getBooleanExtra(IS_SWITCH_NIGHT_MODE, false));
+
         SettingFragment1 fragment1 = new SettingFragment1();
         FragmentManager fragmentManager = getSupportFragmentManager();
         FragmentTransaction fragmentTransaction = fragmentManager.beginTransaction();
@@ -84,28 +95,39 @@ public class SettingsActivity extends BaseActivity {
                 startActivity(new Intent(mActivity, SecondActivity.class));
             }
         });
+        mSwitchNightMode.setChecked(PrefUtil.isNightMode());
+        mSwitchNightMode.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            @Override
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                PrefUtil.setIsNightMode(isChecked);
+                startMySelf();
+            }
+        });
+
+    }
+
+
+    public void startMySelf() {
+        //方案1
+        HandlerUtil.postDelay(new Runnable() {
+            @Override
+            public void run() {
+                ActivityManager.getActivityManager().recreateAllActivity(SettingsActivity.class);
+//                mSlideBackLayout.lock(true);
+            }
+        }, 100);
+
+//        mSlideBackLayout.lock(true);
+        ActivityManager.getActivityManager().finishActivity(this);
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
+        Intent intent = getIntent();
+        intent.putExtra(IS_SWITCH_NIGHT_MODE, true);
+        startActivity(intent);
 
     }
 
     @OnClick(R.id.tv_logout)
     public void onViewClicked() {
-//        !PrefUtil.isNightMode();
-//        PrefUtil.setIsNightMode(true);
-//        finish();
-//        mActivity.recreate();
-//        ActivityManager.getActivityManager().recreateAllActivity(SettingsActivity.class);
-        LogUtil.d(PrefUtil.isNightMode());
-        LogUtil.d("set night mode");
-
-
-//        ActivityManager.getActivityManager().finishActivity(mActivity);
-//        mActivity.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-//        mActivity.startActivity(mActivity.getIntent());
-
-//        Activity activity = this;
-//        activity.getWindow().setWindowAnimations(android.R.style.WindowAnimationFadeInOut);
-//        activity.overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out);
-//        activity.recreate();
 
     }
 
